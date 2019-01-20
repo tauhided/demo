ef9d740954759dae4166b8794bd68587aa5e9844
Tauhid <itauhided@gmail.com>
2019-01-20 10:40:27 +0600

    sub constructor or class Tauhid extends

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ class Person {
     return `${this.name} has not a job yet!!!`
   }
 }
+
+class Tauhid extends Person {
+  constructor(name, job, location) {
+    super(name, job)
+    this.name = name;
+    this.job = job;
+    this.location = location;
+  }
+
+  print() {
+    return `${this.name} lives in ${this.location}`
+  }
+}
